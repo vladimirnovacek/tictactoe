42d49c05877e0c915eb6800b6b8c151a34495b37
vladimirnovacek <novacek.vladimir@gmail.com>
2022-10-18 21:06:26 +0200

    Comments and type hints added.

--- grid_client.py
+++ grid_client.py
@@ -4,28 +4,42 @@ import pygame.draw
 
 
 class Grid:
+    """
+    Grid holds informations of a current state of a game grid and draws it to a surface.
+    """
 
     def __init__(self):
-        self.resources_folder = "resources"
+        self._resources_folder: str = "resources"
 
-        self.image_x = pygame.image.load(os.path.join(self.resources_folder, "x.png"))
-        self.image_o = pygame.image.load(os.path.join(self.resources_folder, "o.png"))
-        self.lines = (
+        self._image_x: pygame.Surface = pygame.image.load(os.path.join(self._resources_folder, "x.png"))
+        self._image_o: pygame.Surface = pygame.image.load(os.path.join(self._resources_folder, "o.png"))
+
+        self._lines: tuple[tuple[tuple[int, int], ...], ...] = (
             ((200, 0), (200, 600)),
             ((400, 0), (400, 600)),
             ((0, 200), (600, 200)),
             ((0, 400), (600, 400))
         )
-        self.grid = [[0 for _ in range(3)] for _ in range(3)]
+        self._grid: list[list[int, ...], ...] = [[0 for _ in range(3)] for _ in range(3)]
+
+        self.winner: int = 0
+        self.game_over: bool = False
+
+    def reset(self) -> None:
+        """
+        Prepares the grid for a new game.
+        :return:
+        """
+        self._grid = [[0 for _ in range(3)] for _ in range(3)]
         self.winner = 0
         self.game_over = False
 
-    def reset(self):
-        self.grid = [[0 for _ in range(3)] for _ in range(3)]
-        self.winner = 0
-        self.game_over = False
-
-    def set_winner(self, player):
+    def set_winner(self, player: int) -> None:
+        """
+        Sets a winner.
+        :param player: 0 for empty, 1 for "X", 2 for "O"
+        :return:
+        """
         if player == 1:
             self.winner = "X"
         elif player == 2:
@@ -33,25 +47,43 @@ class Grid:
         self.game_over = True
 
     def set_cell(self, xx: int, yy: int, value: int) -> None:
-        self.grid[xx][yy] = value
+        """
+        Puts a symbol by the given value to the given cell.
+        :param xx: Column
+        :param yy: Row
+        :param value: 0 for empty, 1 for "X", 2 for "O"
+        :return:
+        """
+        self._grid[xx][yy] = value
 
     @staticmethod
-    def get_cell_coords(xx, yy):
+    def _get_cell_coords(xx: int, yy: int):
         return xx * 200, yy * 200
 
     @staticmethod
-    def get_cell_indexes(x, y):
+    def get_cell_indexes(x: int, y: int) -> tuple[int, ...]:
+        """
+        Transfers mouse coorditates to cell coordinates.
+        :param x: Width
+        :param y: Height
+        :return: Cell coordinates (column, row)
+        """
         return tuple(i // 200 for i in (x, y))
 
-    def draw(self, surface):
-        for column in self.lines:
+    def draw(self, surface: pygame.Surface) -> None:
+        """
+        Draws grid lines and symbols to the surface.
+        :param surface: Surface to draw
+        :return:
+        """
+        for column in self._lines:
             pygame.draw.line(surface, (200, 200, 200), column[0], column[1], 2)
-        for xx, column in enumerate(self.grid):
+        for xx, column in enumerate(self._grid):
             for yy, cell in enumerate(column):
                 if cell == 1:
-                    self.draw_to_cell(xx, yy, surface, self.image_x)
+                    self._draw_to_cell(xx, yy, surface, self._image_x)
                 elif cell == 2:
-                    self.draw_to_cell(xx, yy, surface, self.image_o)
+                    self._draw_to_cell(xx, yy, surface, self._image_o)
 
-    def draw_to_cell(self, xx, yy, surface: pygame.Surface, image: pygame.Surface):
-        surface.blit(image, self.get_cell_coords(xx, yy))
+    def _draw_to_cell(self, xx: int, yy: int, surface: pygame.Surface, image: pygame.Surface):
+        surface.blit(image, self._get_cell_coords(xx, yy))
